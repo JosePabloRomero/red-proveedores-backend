444b93f3bd78c8de52c39fde2006336733764fe5
JohanSR <johaneldiver.jesr@gmail.com>
2020-10-31 16:51:43 -0500

    Primeros avances de productos

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controladorUsuarios = require('../controllers/usuarios.controller');
 const controladorProveedores = require('../controllers/proveedores.controller');
+const controladorProductos = require('../controllers/productos.controller');
 
 router.get('/api/v1', (request, response) => {
     response.send("Hola Mundo");
@@ -18,5 +19,11 @@ router
     .post('/api/v1/proveedores', controladorProveedores.saveProveedor)
     .put('/api/v1/proveedores/:id', controladorProveedores.updateProveedor)
     .delete('/api/v1/proveedores/:id', controladorProveedores.deleteProveedor);
+
+router
+    .get('/api/v1/Productos', controladorProductos.getProductos)
+    .post('/api/v1/Productos', controladorProductos.saveProductos)
+    .put('/api/v1/Productos/:id', controladorProductos.updateProductos)
+    .delete('/api/v1/Productos/:id', controladorProductos.deleteProductos);    
 
 module.exports = router;
