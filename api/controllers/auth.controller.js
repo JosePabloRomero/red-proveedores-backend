8c9689d1dc86e703f9f1ebf835fedf9c985350d6
Jose Pablo Romero <josepablo.developer@gmail.com>
2020-11-04 17:46:02 -0500

    Se cambio el login por id, a login por email. Se solucionó un pequeño error en auth contoller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -16,22 +16,22 @@ const login = async (request, response) => {
         switch (id) {
             //Caso 1: rol: usuario
             case "1":
-                sql += "FROM usuarios u WHERE u.id=$1 and clave=md5($2)";
+                sql += "FROM usuarios u WHERE u.email=$1 and clave=md5($2)";
                 break;
             //Caso 2: rol: proveedor
             case "2":
-                sql += "FROM usuarios u WHERE u.id=$1 and clave=md5($2)";
+                sql += "FROM proveedores u WHERE u.email=$1 and clave=md5($2)";
                 break;
             //Caso 3: rol: administrador
             case "3":
-                sql += "FROM usuarios u WHERE u.id=$1 and clave=md5($2)";
+                sql += "FROM administradores u WHERE u.email=$1 and clave=md5($2)";
                 break;
 
             default:
                 break;
         }
         let body = request.body;
-        let values = [body.id, body.clave];
+        let values = [body.email, body.clave];
         let responseDB = await _ServicePg.execute(sql, values);
         let rowCount = responseDB.rowCount
         if (rowCount == 1) {
@@ -39,8 +39,9 @@ const login = async (request, response) => {
             responseJSON.message = 'El usuario se encuentra en la base de datos';
             responseJSON.info = jwt.createToken(user);
             response.send(responseJSON);
-        } else {
-            responseJSON.message = 'El usuario no existe';
+        } else {            
+            responseJSON.ok = false;
+            responseJSON.message = 'Error al obtener el usuario';
             responseJSON.info = [];
             response.status(404).send(responseJSON);
         }
